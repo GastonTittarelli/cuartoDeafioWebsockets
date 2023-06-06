b11f5edb238249895af6c197a46e48d9aa321a32
GastonTittarelli <116087636+GastonTittarelli@users.noreply.github.com>
2023-06-06 16:18:15 -0300

    corrección en los productos en tiempo real

--- app.js
+++ app.js
@@ -3,21 +3,26 @@ import productRouter from "./src/routes/products.js";
 import cartRouter from "./src/routes/cart.js";
 import handlebars from "express-handlebars";
 import ProductManager from "./src/datos/ManagerProducts.js";
-
 import http from "http";
-import socketIO from "socket.io";
-// import { Server } from "socket.io";
+import { Server } from "socket.io";
 
 const manager = new ProductManager();
 
 const app = express();
-const serve = http.createServer(app);
-const io = socketIO(serve);
+
+const serve = http.Server(app);
+const io = new Server(serve);
 
 app.engine("handlebars", handlebars.engine());
 app.set("views", "./views");
 app.set("view engine", "handlebars")
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static("public"));
+
+app.use("/api/product", productRouter);
+app.use("/api/cart", cartRouter);
 
 
 app.get("/hbs", async (req, res) => {
@@ -33,18 +38,12 @@ io.on("connection", (socket) => {
     socket.emit("productos", manager.getProducts());
 });
 
-app.get('/realtimeproducts', (req, res) => {
-    res.render('realtimeproducts');
+app.get('/realtimeproducts', async (req, res) => {
+    let productos = await manager.getProducts();
+    res.render('realtimeproducts', { productos }); 
 });
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
-app.use(express.static("public"));
-
-app.use("/api/product", productRouter);
-app.use("/api/cart", cartRouter);
 
 const PORT = 8080;
 const server = app.listen(PORT, () => console.log(`Servidor corriendo en puerto: ${server.address().port}`))
 server.on("error", error => console.log(error))
-
